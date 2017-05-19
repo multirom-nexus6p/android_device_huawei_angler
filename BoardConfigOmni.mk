013078627a8902690c88e2d54a86e7a52faed54d
Nolen Johnson <johnsonnolen@gmail.com>
2017-05-18 23:08:28 -0400

    Move TWRP flags to common boardconfig

--- BoardConfigOmni.mk
+++ BoardConfigOmni.mk
@@ -37,13 +37,3 @@ TARGET_INCREASES_COLDBOOT_TIMEOUT := true
 
 # not really for recovery... but healthd still uses it
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
-
-# TWRP
-TW_THEME := portrait_hdpi
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-BOARD_SUPPRESS_SECURE_ERASE := true
-RECOVERY_SDCARD_ON_DATA := true
-BOARD_HAS_NO_REAL_SDCARD := true
-TW_INCLUDE_CRYPTO := true
-TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
-TARGET_RECOVERY_QCOM_RTC_FIX := true
